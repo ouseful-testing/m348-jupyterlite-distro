9b10af161f2d2db8c3d539b4a31b08cf9f6cbb87
Tony Hirst <tony.hirst@gmail.com>
2024-09-18 14:53:51 +0100

    Update distribution

--- static/files/packages/M348/R/M348-package.R
+++
@@ -1,9 +0,0 @@
-#' @details
-#' Collection of datasets and functions for use when studying M348 Applied statistical modelling
-#'
-#' @keywords internal
-"_PACKAGE"
-#> [1] "_PACKAGE"
-
-#' @alias m348
-
