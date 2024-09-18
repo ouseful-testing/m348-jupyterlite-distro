2f1240b8e1fbd140c62e30a5ecd0d6777e16d258
Tony Hirst <tony.hirst@gmail.com>
2024-09-18 14:53:29 +0100

    Add ability to seve local dir for read-csv of local files
    Also add CORS support so eg we can read localhost from 127.0.0.1
    
    We can make this more secure by commenting out the CORS statement in app.py

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # m348-jupyterlite-distro
 Builder for pyinstaller distributions - flask + jupyterlite bundle exe
+
+
+Based on: `pyinstaller --onefile --add-data "static:static" --name m348-jupyterlite app.py`
+
+
+The web app also serves files from the `M348-24J` directory, so we can load datafiles in:
+
+```R
+shared_url = function (filename) {
+    PORTNUMBER <- if (exists("PORTNUMBER")) PORTNUMBER else 8348
+    paste0("http://localhost:",PORTNUMBER,"/share/",filename)
+}
+
+# The server also serves files from the "M348-24J"
+# in the user's home directory.
+
+#For example, if I have a data file "testdata.csv"
+# in a folder called "data"
+# inside a folder called EMA
+# inside my "M348-24J" folder in my home directory
+# I can set a path to it inside the "M348-24J" folder as:
+DESKTOP_HOMEDIR_DATA_FILE = "EMA/data/testdata.csv"
+
+# Now read the data file in:
+shared_data = read.csv(shared_url(DESKTOP_HOMEDIR_DATA_FILE))
+
+head(shared_data)
+```
--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, send_from_directory
+from flask_cors import CORS
 import os
+from pathlib import Path
 
 app = Flask(__name__)
 
+# SISE use:
+# If we need to tighten this down further we can omit CORS
+CORS(app)
+
+
+SHARED_DIR = Path(os.path.expanduser("~")) / "M348-24J"
+if not os.path.exists(SHARED_DIR):
+    os.makedirs(SHARED_DIR)
 
 @app.route("/")
 def index():
@@ -12,6 +22,10 @@ def index():
 @app.route("/<path:path>")
 def serve_static(path):
     return send_from_directory("static", path)
+
+@app.route("/share/<path:path>")
+def serve_desktop_files(path):
+    return send_from_directory(SHARED_DIR, path)
 
 
 def open_browser(port):
@@ -26,7 +40,7 @@ if __name__ == "__main__":
     # Prompt the user for a port number
     while True:
         try:
-            port = int(input("Enter the port number to run the server on: "))
+            port = int(input("Enter the port number to run the server on (e.g. 8348, or 0 for random allocation): "))
             if 1024 <= port <= 65535 or port==0:
                 break
             else:
